3dc67d1c95842e29499e2cb8e7a10efa43697808
jat4625 <jatorr98@gmail.com>
2020-01-30 11:01:21 -0600

    Completed library and example sketch (see pressure folder)

--- pressure/ms5540c.h
+++ pressure/ms5540c.h
@@ -7,13 +7,15 @@
 // include library:
 #include <SPI.h>
 
+
+const int MCLK = 9; //CLOCK
 class MS5540C{
   public:
     // Calibration words 1-4
-    long cw1;
-    long cw2;
-    long cw3;
-    long cw4;
+    unsigned int cw1;
+    unsigned int cw2;
+    unsigned int cw3;
+    unsigned int cw4;
     // Calibration factors 1-6
     long c1;
     long c2;
@@ -22,51 +24,50 @@ class MS5540C{
     long c5;
     long c6;
     // Pressure
-    int rawPress;
+    unsigned int rawPress;
     int Pressure;
     int secondOrderPressure;
     // Temperature
-    int rawtemp;
-    int temp;
-    // Pins
-    int MOSI; // Din
-    int MISO; // Dout
-    int SCLK;
-    int MCLK;
+    unsigned int rawtemp;
+    float temp;
 
     // Second order pressure
     long pcomp;
     long ptemp;
 
     void resetSensor();
-    void temperature();
-    void rawPressure();
-    void compPressure();
+    void spiCheck();
+    void measure(int raw);
     void factorsFromWords();
     void secondDegCompPressure();
-    void calibration();
-    MS5540C(int MOSI, int MISO, int SCLK, int MCLK); //constructor
+    void calibration(int readOut);
+    MS5540C(); //constructor
+    //MS5540C();
 
 };
 
 // default constructor
-//MS5540C::MS5540C(){
-  //MS5540C(int MOSI = 11, int MISO = 12, int SCLK = 13, int MCLK = 9);
-//}
+/*MS5540C::MS5540C(){
+  MS5540C(int MOSI = 11, int MISO = 12, int SCLK = 13, int MCLK = 9);
+}*/
 // Parameterized constructor
-MS5540C::MS5540C(int MOSI = 11, int MISO = 12, int SCLK = 13, int MCLK = 9) {
-  MOSI = MOSI;
-  MISO = MISO;
-  SCLK = SCLK;
-  MCLK = MCLK;
+MS5540C::MS5540C() {
   Serial.begin(9600);
   SPI.begin(); //see SPI library details on arduino.cc for details
   SPI.setBitOrder(MSBFIRST);
   SPI.setClockDivider(SPI_CLOCK_DIV32); //divide 16 MHz to communicate on 500 kHz
   pinMode(MCLK, OUTPUT);
+  Serial.print("MS5540C initialized\n");
   delay(100);
 }
-
+void MS5540C::spiCheck(){
+  SPI.begin(); //see SPI library details on arduino.cc for details
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV32); //divide 16 MHz to communicate on 500 kHz
+  pinMode(MCLK, OUTPUT);
+  TCCR1B = (TCCR1B & 0xF8) | 1 ; //generates the MCKL signal
+  analogWrite (MCLK, 128) ;
+}
 void MS5540C::resetSensor() //this function keeps the sketch a little shorter
 {
   SPI.setDataMode(SPI_MODE0);
@@ -75,7 +76,7 @@ void MS5540C::resetSensor() //this function keeps the sketch a little shorter
   SPI.transfer(0x40);
 }
 
-void MS5540C::calibration()
+void MS5540C::calibration(int readOut = 0)
 {
   TCCR1B = (TCCR1B & 0xF8) | 1 ; //generates the MCKL signal
   analogWrite (MCLK, 128) ;
@@ -92,10 +93,13 @@ void MS5540C::calibration()
   result1 = result1 << 8; //shift returned byte
   inbyte1 = SPI.transfer(0x00); //send dummy byte to read second byte of word
   result1 = result1 | inbyte1; //combine first and second byte of word
-  Serial.print("Calibration word 1 = ");
-  Serial.print(result1,HEX);
-  Serial.print(" ");
-  Serial.println(result1);
+  if (readOut){
+    Serial.print("Calibration word 1 = ");
+    Serial.print(result1,HEX);
+    Serial.print(" ");
+    Serial.println(result1);
+  }
+ 
   cw1 = result1;
 
   resetSensor(); //resets the sensor
@@ -110,11 +114,14 @@ void MS5540C::calibration()
   result2 = result2 <<8;
   inbyte2 = SPI.transfer(0x00);
   result2 = result2 | inbyte2;
-  Serial.print("Calibration word 2 = ");
-  Serial.print(result2,HEX);
-  Serial.print(" ");
-  Serial.println(result2);
-  cw2 = result2
+  if (readOut){
+    Serial.print("Calibration word 2 = ");
+    Serial.print(result2,HEX);
+    Serial.print(" ");
+    Serial.println(result2);
+  }
+  
+  cw2 = result2;
 
   resetSensor(); //resets the sensor
 
@@ -128,11 +135,14 @@ void MS5540C::calibration()
   result3 = result3 <<8;
   inbyte3 = SPI.transfer(0x00);
   result3 = result3 | inbyte3;
-  Serial.print("Calibration word 3 = ");
-  Serial.print(result3,HEX);
-  Serial.print(" ");
-  Serial.println(result3);
-  cw3 = result3
+  if (readOut){
+    Serial.print("Calibration word 3 = ");
+    Serial.print(result3,HEX);
+    Serial.print(" ");
+    Serial.println(result3);
+  }
+  
+  cw3 = result3;
   resetSensor(); //resets the sensor
 
   //Calibration word 4; see comments on calibration word 1
@@ -145,49 +155,55 @@ void MS5540C::calibration()
   result4 = result4 <<8;
   inbyte4 = SPI.transfer(0x00);
   result4 = result4 | inbyte4;
-  Serial.print("Calibration word 4 = ");
-  Serial.print(result4,HEX);
-  Serial.print(" ");
-  Serial.println(result4);
-  cw4 = result4
+  if ( readOut){
+    Serial.print("Calibration word 4 = ");
+    Serial.print(result4,HEX);
+    Serial.print(" ");
+    Serial.println(result4);
+  }
+  
+  cw4 = result4;
 
   //now we do some bitshifting to extract the calibration factors
   //out of the calibration words;
-  c1 = (result1 >> 1) & 0x7FFF;
-  c2 = ((result3 & 0x003F) << 6) | (result4 & 0x003F);
-  c3 = (result4 >> 6) & 0x03FF;
-  c4 = (result3 >> 6) & 0x03FF;
-  c5 = ((result1 & 0x0001) << 10) | ((result2 >> 6) & 0x03FF);
-  c6 = result2 & 0x003F;
-
-  Serial.print("c1 = ");
-  Serial.println(c1);
-  Serial.print("c2 = ");
-  Serial.println(c2);
-  Serial.print("c3 = ");
-  Serial.println(c3);
-  Serial.print("c4 = ");
-  Serial.println(c4);
-  Serial.print("c5 = ");
-  Serial.println(c5);
-  Serial.print("c6 = ");
-  Serial.println(c6);
+  c1 = cw1 >> 1;
+  c2 = ((cw3 & 0x3F) << 6) | ((cw4 & 0x3F));
+  c3 = (cw4 >> 6) ;
+  c4 = (cw3 >> 6);
+  c5 = (cw2 >> 6) | ((cw1 & 0x1) << 10);
+  c6 = cw2 & 0x3F;
+  if (readOut){
+    Serial.print("c1 = ");
+    Serial.println(c1);
+    Serial.print("c2 = ");
+    Serial.println(c2);
+    Serial.print("c3 = ");
+    Serial.println(c3);
+    Serial.print("c4 = ");
+    Serial.println(c4);
+    Serial.print("c5 = ");
+    Serial.println(c5);
+    Serial.print("c6 = ");
+    Serial.println(c6);
+  }
+  
 
   resetSensor(); //resets the sensor
 }
 
 // get appropriate factors from calibration words
 void MS5540C::factorsFromWords(){
-  c1 = (cw1 >> 1) & 0x7FFF;
-  c2 = ((cw3 & 0x003F) << 6) | (cw4 & 0x003F);
-  c3 = (cw4 >> 6) & 0x03FF;
-  c4 = (cw3 >> 6) & 0x03FF;
-  c5 = ((cw1 & 0x0001) << 10) | ((cw2 >> 6) & 0x03FF);
-  c6 = cw2 & 0x003F;
+  c1 = cw1 >> 1;
+  c2 = ((cw3 & 0x3F) << 6) | ((cw4 & 0x3F));
+  c3 = (cw4 >> 6) ;
+  c4 = (cw3 >> 6);
+  c5 = (cw2 >> 6) | ((cw1 & 0x1) << 10);
+  c6 = cw2 & 0x3F;
   resetSensor();
 }
 
-void MS5540C::rawPressure(void){
+void MS5540C::measure(int raw = 0){
+    spiCheck();
     unsigned int presMSB = 0; //first byte of value
     unsigned int presLSB = 0; //last byte of value
     unsigned int D1 = 0;
@@ -199,15 +215,15 @@ void MS5540C::rawPressure(void){
     presMSB = presMSB << 8; //shift first byte
     presLSB = SPI.transfer(0x00); //send dummy byte to read second byte of value
     D1 = presMSB | presLSB; //combine first and second byte of value
-    Serial.print("D1 - Pressure raw = ");
-    Serial.println(D1);
+    if (raw){
+      Serial.print("D1 - Pressure raw = ");
+      Serial.println(D1);
+    }
+    
     rawPress = D1;
 
     resetSensor(); //resets the sensor
-  }
-
-
-void MS5540C::temperature(){
+  
   unsigned int tempMSB = 0; //first byte of value
   unsigned int tempLSB = 0; //last byte of value
   unsigned int D2 = 0;
@@ -219,34 +235,67 @@ void MS5540C::temperature(){
   tempMSB = tempMSB << 8; //shift first byte
   tempLSB = SPI.transfer(0x00); //send dummy byte to read second byte of value
   D2 = tempMSB | tempLSB; //combine first and second byte of value
-  Serial.print("D2 - Temperature raw = ");
-  Serial.println(D2); //voila!
+  if (raw){
+    Serial.print("D2 - Temperature raw = ");
+    Serial.println(D2);
+  }
+  
   rawtemp = D2;
-  resetSensor();
-}
-void MS5540C::compPressure(){
+  
   //calculation of the real values by means of the calibration factors and the maths
   //in the datasheet. const MUST be long
+  
   const long UT1 = (c5 << 3) + 20224;
-  const long dT = rawtemp - UT1;
+  const long dT = D2 - UT1;
   const long TEMP = 200 + ((dT * (c6 + 50)) >> 10);
   const long OFF  = (c2 * 4) + (((c4 - 512) * dT) >> 12);
   const long SENS = c1 + ((c3 * dT) >> 10) + 24576;
-  const long X = (SENS * (rawPress - 7168) >> 14) - OFF;
+  const long X = (SENS * (D1 - 7168) >> 14) - OFF;
   long PCOMP = ((X * 10) >> 5) + 2500;
   float TEMPREAL = TEMP/10;
+  float PCOMPREAL = PCOMP / 10;
   float PCOMPHG = PCOMP * 750.06 / 10000; // mbar*10 -> mmHg === ((mbar/10)/1000)*750/06
   temp = TEMPREAL;
   pcomp = PCOMP;
   ptemp = TEMP;
-
+  /*Serial.print("===========\n");
+  Serial.print(PCOMP);
+  Serial.print("===========\n");*/
+  if (raw){
+    Serial.print("c1 = ");
+    Serial.println(c1);
+    Serial.print("c2 = ");
+    Serial.println(c2);
+    Serial.print("c3 = ");
+    Serial.println(c3);
+    Serial.print("c4 = ");
+    Serial.println(c4);
+    Serial.print("c5 = ");
+    Serial.println(c5);
+    Serial.print("c6 = ");
+    Serial.println(c6);
+    
+    Serial.print("UT1 = ");
+    Serial.println(UT1);
+    Serial.print("dT = ");
+    Serial.println(dT);
+    Serial.print("TEMP = ");
+    Serial.println(TEMP);
+    Serial.print("OFFP = ");
+    Serial.println(OFF);
+    Serial.print("SENS = ");
+    Serial.println(SENS);
+    Serial.print("X = ");
+    Serial.println(X);
+  }
+  
+  
   Serial.print("Real Temperature in C = ");
   Serial.println(TEMPREAL);
   Serial.print("Compensated pressure in mbar = ");
-  Serial.println(PCOMP);
+  Serial.println(PCOMPREAL);
   Serial.print("Compensated pressure in mmHg = ");
   Serial.println(PCOMPHG);
-  resetSensor();
 }
 void MS5540C::secondDegCompPressure(){
   //2-nd order compensation only for T < 20°C or T > 45°C
@@ -281,5 +330,4 @@ void MS5540C::secondDegCompPressure(){
     Serial.print("2-nd Compensated pressure in mmHg = ");
     Serial.println(PCOMPHG2);
   }
-  resetSensor();
 }
--- pressure/pressure.ino
+++ pressure/pressure.ino
@@ -1,10 +1,22 @@
 #include "ms5540c.h"
+
+
 void setup() {
   // put your setup code here, to run once:
-  MS5540C snr;
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  static MS5540C snr;
+  /*snr.c1 = 21848;
+  snr.c2 = 2125;
+  snr.c3 = 718;
+  snr.c4 = 609;
+  snr.c5 = 1028;
+  snr.c6 = 26;*/
+  snr.calibration();
+  snr.measure();
+  delay(1000);
 
 }
