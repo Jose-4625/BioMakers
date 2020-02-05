a8920b1be7abf29785a6e26f6898cb1bd71973de
jat4625 <jatorr98@gmail.com>
2020-02-05 14:53:49 -0600

    Added barebones OLED script

---
+++ OLED/OLED.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // I2C Address: 0x3C
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+}
+void loop() {
+
+  // put your main code here, to run repeatedly:
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+
+  // Display static text
+  display.print(); //Put things to be displayed in this statement
+  display.display();
+
+  delay(100);
+
+}
